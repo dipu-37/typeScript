0608aabd2cda094c616503137085e177a1408ddc
dipu-37 <razibul1105nstu@gmail.com>
2024-10-25 21:42:12 +0600

    object

---
+++ src/object.ts
@@ -0,0 +1,17 @@
+
+const user : {
+    //readonly company: string:
+    company: "programming hero";
+    name: string;
+    age: number;
+    ismarried : boolean;
+    wife?: string;
+}=  {
+    company: "programming hero",
+    name : "dipu",
+    age: 43,
+    ismarried: true,
+   // wife: "tahira"
+}
+
+user.company = "programming hero"
